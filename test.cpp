dcc74308be0b9f7b92b6fb43025bc13e1e3dfd19
AhmadMujtaba200210 <cadeta6@gmail.com>
2022-01-10 23:32:58 +0500

    commites

--- test.cpp
+++ test.cpp
@@ -7,9 +7,8 @@ using namespace std;
 void read_record()
 {
 	fstream fin;
-
 	// Open an existing file
-	fin.open("Applications.csv", ios::in);
+	fin.open("Applications.txt", ios::in);
 	// Read the Data from the file
 	// as String Vector
 	vector<string> row;
@@ -20,26 +19,14 @@ if(fin.is_open()){
 
 		// read an entire row and
 		// store it in a string variable 'line'
-		getline(fin, line);
-
-		// used for breaking words
-		stringstream s(line);
-
-		// read every column data of a row and
-		// store it in a string variable, 'word'
-		while (getline(s, word, ',')) {
-
-			// add all the column data
-			// of a row to a vector
-			row.push_back(word);
-		}
+		getline(fin, line,',');
 			count = 1;
 			//cout << " " << row[0] << " : \n";
 			cout << "Name: " << row[0] << "\n";
-			cout << "Maths: " << row[1] << "\n";
-			cout << "Physics: " << row[2] << "\n";
-			cout << "Chemistry: " << row[3] << "\n";
-			cout << "Biology: " << row[4] << "\n";
+//			cout << "Maths: " << row[1] << "\n";
+//			cout << "Physics: " << row[2] << "\n";
+//			cout << "Chemistry: " << row[3] << "\n";
+//			cout << "Biology: " << row[4] << "\n";
 	}
 	if (count == 0)
 		cout << "Record not found\n";
